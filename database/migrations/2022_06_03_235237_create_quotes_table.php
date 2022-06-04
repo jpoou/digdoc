9c3a8300bd0dc0d140d6da1c921450dbbbcd35f4
Juan Carlos Morales <juancarlosmorales85@gmail.com>
2022-06-04 12:29:16 -0600

    feat(add): active of soft delete Patient.php

---
+++ database/migrations/2022_06_03_235237_create_quotes_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Models\User;
+use App\Models\Branch;
+use App\Models\Patient;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('quotes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Patient::class);
+            $table->foreignIdFor(Branch::class);
+            $table->foreignIdFor(User::class, 'creator_id');
+            $table->enum('status', ['open', 'progress', 'complete']);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('quotes');
+    }
+};
