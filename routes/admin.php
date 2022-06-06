aa6d9f183ee33207298581aa761a7fd5d58c402b
Juan Carlos Morales <juancarlosmorales85@gmail.com>
2022-06-06 11:45:09 -0600

    feat(add): clearing routing of admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('home', 'components.home')->name('name')->middleware('auth');
-
 Route::resource('branches', \App\Http\Controllers\Admin\BranchController::class);
 Route::resource('patients', \App\Http\Controllers\Admin\PatientsController::class);
 Route::resource('appointments', \App\Http\Controllers\Admin\PatientAppointmentController::class)->only('show', 'store');
