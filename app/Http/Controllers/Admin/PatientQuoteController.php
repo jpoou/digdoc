617a26ad53e17aa98c05aa94b34b129cb209c84e
Juan Carlos Morales <juancarlosmorales85@gmail.com>
2022-06-06 10:58:29 -0600

    feat(add): create controller for create quote

---
+++ app/Http/Controllers/Admin/PatientQuoteController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PatientQuoteController extends Controller
+{
+    public function show()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+}
