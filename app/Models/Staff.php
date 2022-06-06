29825c53142125ffb86dc6067d51e5df7d676355
Juan Carlos Morales <juancarlosmorales85@gmail.com>
2022-06-06 13:49:59 -0600

    feat(add): staff structuring

---
+++ app/Models/Staff.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Staff extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'title',
+        'name',
+        'surname',
+        'branch_id'
+    ];
+}
---
+++ database/migrations/2022_06_06_133621_create_staff_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Models\Branch;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('staffs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 20);
+            $table->string('name');
+            $table->string('surname');
+            $table->foreignIdFor(Branch::class)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('staffs');
+    }
+};
