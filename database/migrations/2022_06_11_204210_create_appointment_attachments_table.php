a8eecb466e1235c713887165004d686f73984e6b
Juan Carlos Morales <juancarlosmorales85@gmail.com>
2022-06-11 20:51:00 -0600

    feat(add): structure of AppointmentAttachment for history

---
+++ database/migrations/2022_06_11_204210_create_appointment_attachments_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\Appointment;
+use App\Models\Attachment;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('appointment_attachment', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Appointment::class);
+            $table->foreignIdFor(Attachment::class);
+            $table->enum('type', ['record', 'diagnosis', 'prescription', 'study']);
+            $table->integer('quantity');
+            $table->text('indications');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('appointment_attachment');
+    }
+};
