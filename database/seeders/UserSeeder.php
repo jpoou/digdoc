251c78a240c410d78483ccaff80d8c291b7986dc
Juan Carlos Morales <juancarlosmorales85@gmail.com>
2022-08-16 16:55:20 -0600

    feat(add): refresh UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -9,9 +9,11 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'Juan Carlos Morales',
+            'email' => 'admin@devpoou.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ]);
     }
 }
