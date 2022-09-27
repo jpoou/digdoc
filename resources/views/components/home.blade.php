855c047059796a7bfb28436dbe900e9aa0bcfe41
Juan Carlos Morales <juancarlosmorales85@gmail.com>
2022-09-26 19:54:26 -0600

    feat(add): href to home in dashboard

--- resources/views/components/home.blade.php
+++ resources/views/components/home.blade.php
@@ -6,7 +6,7 @@
                     <div class="page-title">Dashboard</div>
                 </div>
                 <ol class="breadcrumb page-breadcrumb pull-right">
-                    <li><i class="fa fa-home"></i>&nbsp;<a class="parent-item" href="index.html">Home</a>&nbsp;<i
+                    <li><i class="fa fa-home"></i>&nbsp;<a class="parent-item" href="{{ route('dashboard') }}">Home</a>&nbsp;<i
                             class="fa fa-angle-right"></i>
                     </li>
                     <li class="active">Dashboard</li>
