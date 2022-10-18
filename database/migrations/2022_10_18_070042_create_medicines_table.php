6f0db0e70c85d290289866201b74cb2bcb331b55
Juan Carlos Morales <juancarlosmorales85@gmail.com>
2022-10-18 08:30:26 -0600

    feat(add): implement unique name with slug column

--- database/migrations/2022_10_18_070042_create_medicines_table.php
+++ database/migrations/2022_10_18_070042_create_medicines_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
         Schema::create('medicines', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('slug');
+            $table->string('slug')->unique();
             $table->string('dose');
             $table->string('frequency');
             $table->string('duration');
