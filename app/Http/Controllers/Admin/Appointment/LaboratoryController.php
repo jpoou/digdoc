b02e9676816a617908347fec25d46b57a1242d75
Juan Carlos Morales <juancarlosmorales85@gmail.com>
2022-10-22 00:51:21 -0600

    feat(add): cleaning controller LaboratoryController.php

--- app/Http/Controllers/Admin/Appointment/LaboratoryController.php
+++ app/Http/Controllers/Admin/Appointment/LaboratoryController.php
@@ -10,18 +10,6 @@ use Illuminate\Support\Facades\Request;
 
 class LaboratoryController extends Controller
 {
-    public function index()
-    {
-        return view('modules.branches.index', [
-            'laboratories' => Laboratory::all()
-        ]);
-    }
-
-    public function create()
-    {
-        return view('modules.branches.create');
-    }
-
     public function store(Appointment $appointment, LaboratoryRequest $request)
     {
         $data = $request->validated();
@@ -32,24 +20,5 @@ class LaboratoryController extends Controller
         Laboratory::create($data);
 
         return redirect()->route('appointment.prescription.index', $appointment)->with('message', 'Creado exitosamente');
-    }
-
-    public function edit(Laboratory $branch)
-    {
-        return view('modules.branches.edit', [
-            'branch' => $branch
-        ]);
-    }
-
-    public function update(LaboratoryRequest $request, Laboratory $branch)
-    {
-        $branch->update($request->validated());
-        return redirect()->route('laboratories.index')->with('message', 'Creado exitosamente');
-    }
-
-    public function destroy(Laboratory $branch)
-    {
-        $branch->delete();
-        return redirect()->route('laboratories.index')->with('message', 'Eliminado exitosamente');
     }
 }
